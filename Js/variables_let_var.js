2f52ab62c300c77e69dfa7d0b597100bebfdc700
Hugo GG <mategomez04.hg@gmail.com>
2024-08-08 12:29:40 -0600

    Subiendo los ultimos cambios de los archivos

---
+++ Js/variables_let_var.js
@@ -0,0 +1,12 @@
+/*
+El problema de utilizar el var es que sobrescribe las funciones del lenguaje de JS 
+
+
+let saludar = "Hola mundo";
+var saludar1 = "Hello World";
+console.log(window);
+let alert = "alerta";
+alert(saludar);
+*/
+alert("hola mundo");
+var alert = "Hello world"
